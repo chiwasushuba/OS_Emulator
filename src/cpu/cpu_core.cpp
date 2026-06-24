05461a82722ea6ab06c7e008df4502c4c74ba4df
KaoM3 <rafaeldcbernandino@gmail.com>
2026-06-25 02:02:17 +0800

    feat: add cpu component

--- src/cpu/cpu_core.cpp
+++ src/cpu/cpu_core.cpp
@@ -1,8 +1,36 @@
-// TODO: Implement CPU
-// Parameters:
-//  - num_cores
-// - ready_queue
-// - wait_queue (for sleeping tasks)
-// Methods:
-// - add_process
-// - remove_process
+#include "cpu.h"
+
+CPUCore::CPUCore(int id) : core_id(id) {}
+
+int CPUCore::get_id() const {
+    return core_id;
+}
+
+bool CPUCore::is_idle() const {
+    return current_process == nullptr;
+}
+
+void CPUCore::assign_process(Process* process) {
+    current_process = process;
+
+    if (current_process) {
+        current_process->core_id = core_id;
+        current_process->state = ProcessState::RUNNING;
+    }
+}
+
+Process* CPUCore::get_process() const {
+    return current_process;
+}
+
+void CPUCore::remove_process() {
+    current_process = nullptr;
+}
+
+bool CPUCore::tick(LogEntry& log) {
+    if (current_process == nullptr) {
+        return false;
+    }
+
+    return current_process->execute_next_instruction(log);
+}
