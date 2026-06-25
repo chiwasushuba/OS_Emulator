de6b0a12ce4e3e7f018452296801521c6d8e7415
KaoM3 <rafaeldcbernandino@gmail.com>
2026-06-25 14:17:28 +0800

    fix: make default config match specs

--- src/config/config.h
+++ src/config/config.h
@@ -4,12 +4,12 @@
 constexpr uint64_t MAX_CONFIG_VALUE = 4294967296ULL;
 
 struct Config {
-    int num_cpu = 1;
-    std::string scheduler = "fcfs";
-    uint64_t quantum_cycles = 4;
-    uint64_t batch_process_freq = 8;
-    uint64_t min_ins = 1;
-    uint64_t max_ins = 16;
+    int num_cpu = 4;
+    std::string scheduler = "rr";
+    uint64_t quantum_cycles = 5;
+    uint64_t batch_process_freq = 1;
+    uint64_t min_ins = 1000;
+    uint64_t max_ins = 2000;
     uint64_t delays_per_exec = 0;
 };
 
